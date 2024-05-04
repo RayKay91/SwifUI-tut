759ad4ad656245788d7356bbd00ce4a24744a025
RayKay91 <61206305+RayKay91@users.noreply.github.com>
2024-05-04 21:13:49 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ray Khalid on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
